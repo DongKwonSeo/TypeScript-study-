1c81b8a41e9f6712f3ac39edc4e1236002f5b420
DongKwon <gun100491@gmail.com>
2021-08-01 23:45:57 +0900

    Feat: TS aliases

---
+++ type/1-4-aliases.ts
@@ -0,0 +1,18 @@
+{
+  // Type Aliases
+
+  type Text = string;
+  const name: Text = "ellie";
+  const address: Text = "krea";
+  type NUm = number;
+  type Student = {
+    name: string;
+    age: number;
+  };
+  //   Type 지정하면 그거에 맞게 타입을 사용해야한다
+
+  const student: Student = {
+    name: "rudy",
+    age: 2,
+  };
+}
