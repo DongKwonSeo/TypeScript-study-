7ce8b135bd5e33056841a1479bd3d8cadda2c6a6
DongKwon <gun100491@gmail.com>
2021-08-03 15:36:35 +0900

    Feat: TS Inference

---
+++ type/1-9-inference.ts
@@ -0,0 +1,11 @@
+// Type Inference
+
+{
+  let text = "hello";
+  //  타입 자동으로 적용되므로 let text : string = hello 안 적어도 된다
+  function print(hello: string) {
+    console.log(print);
+  }
+  print("2");
+//   타입을 추론 하는 것보다 , 명시적으로 보여주는 것이 더 좋은 코드라고 말할수 있다 
+}
