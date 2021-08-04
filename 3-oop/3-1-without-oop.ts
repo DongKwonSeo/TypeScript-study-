e97c1147e394d71004819991c5773cb41c935479
DongKwon <gun100491@gmail.com>
2021-08-04 22:24:02 +0900

    Feat: oop 연습 문제

---
+++ 3-oop/3-1-without-oop.ts
@@ -0,0 +1,18 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+  const BEANS_GRAMM_PER_SHOW: number = 7;
+  let coffBeans: number = 0;
+  function makeCoffee(shots: number): CoffeeCup {
+    if (coffBeans < shots * BEANS_GRAMM_PER_SHOW) {
+      throw new Error("not enough");
+    }
+    coffBeans -= shots * BEANS_GRAMM_PER_SHOW;
+    return {
+      shots,
+      hasMilk: false,
+    };
+  }
+}
