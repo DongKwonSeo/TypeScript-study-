6b7f0d4ff8cbc3f00a28cf3ef4e59e3653781748
DongKwon <gun100491@gmail.com>
2021-08-05 23:25:03 +0900

    Feat: class oop

--- 3-oop/3-1-without-oop.ts
+++ 3-oop/3-1-without-oop.ts
@@ -1,18 +1,21 @@
-{
-  type CoffeeCup = {
-    shots: number;
-    hasMilk: boolean;
-  };
-  const BEANS_GRAMM_PER_SHOW: number = 7;
-  let coffBeans: number = 0;
-  function makeCoffee(shots: number): CoffeeCup {
-    if (coffBeans < shots * BEANS_GRAMM_PER_SHOW) {
-      throw new Error("not enough");
-    }
-    coffBeans -= shots * BEANS_GRAMM_PER_SHOW;
-    return {
-      shots,
-      hasMilk: false,
-    };
-  }
-}
+// {
+//   type CoffeeCup = {
+//     shots: number;
+//     hasMilk: boolean;
+//   };
+//   const BEANS_GRAMM_PER_SHOW: number = 7;
+//   let coffBeans: number = 0;
+//   function makeCoffee(shots: number): CoffeeCup {
+//     if (coffBeans < shots * BEANS_GRAMM_PER_SHOW) {
+//       throw new Error("not enough");
+//     }
+//     coffBeans -= shots * BEANS_GRAMM_PER_SHOW;
+//     return {
+//       shots,
+//       hasMilk: false,
+//     };
+//   }
+//   coffBeans += 3 * BEANS_GRAMM_PER_SHOW;
+//   const coffee = makeCoffee(2);
+//   console.log(coffee);
+// }
