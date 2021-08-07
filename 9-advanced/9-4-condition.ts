e2fd07e135af1b1423f4d49d8f89e790e7f98370
DongKwon <gun100491@gmail.com>
2021-08-07 14:13:52 +0900

    Feat: Ts  partial

--- 9-advanced/9-4-condition.ts
+++ 9-advanced/9-4-condition.ts
@@ -1,2 +1,19 @@
 type Chexk<T> = T extends string ? boolean : number;
-type Type = Chexk<string>;  // boolean
+type Type = Chexk<string>; // boolean
+
+type TypeName<T> = T extends string
+  ? "string"
+  : T extends number
+  ? "number"
+  : T extends boolean
+  ? "boolean"
+  : T extends undefined
+  ? "undifined"
+  : T extends Function
+  ? "function"
+  : "object";
+
+type T0 = TypeName<string>;
+("string");
+type T1 = TypeName<"a">;
+type T2 = TypeName<() => void>;
