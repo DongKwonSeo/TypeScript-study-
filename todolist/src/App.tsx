1b6d7128b83ab43e9d20a696fb8a8cac6e78da26
DongKwon <gun100491@gmail.com>
2021-08-11 16:25:10 +0900

    Feat: Header,Input,Button 레이아웃,css 추가

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,10 +1,27 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
+import { ChangeEvent } from "react";
 import "./App.css";
 
 const App: FC = () => {
   return (
     <div className="App">
-      <div className="header"></div>
+      <div className="header">
+        <div className="inputContainer">
+          <input
+            type="text"
+            placeholder="Task.."
+            name="task"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="deadline"
+            placeholder="Deadline (in Days)..."
+            onChange={handleChange}
+          />
+        </div>
+        <button>Add Task</button>
+      </div>
       <div className="todoList"></div>
     </div>
   );
